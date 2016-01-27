742ea76c3eb2fe5d1928022dff26da0fc81e5285
andela-ggikera <githinji.gikera@andela.com>
2016-01-27 17:29:22 +0300

    (fix) Add created_by to models

--- bucketlist/bucketlist/urls.py
+++ bucketlist/bucketlist/urls.py
@@ -1,21 +1,5 @@
-from django.conf.urls import url
-import restapi.views as views
+from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^bucketlists/$',
-        views.BucketlistView.as_view(),
-        name='api.bucketlists'),
-
-    url(r'^bucketlists/(?P<pk>[0-9]+)/$',
-        views.BucketlistDetailView.as_view(),
-        name="api.bucketlist"),
-
-    url(r'^bucketlists/(?P<pk>[0-9]+)/items/$',
-        views.BucketlistItemCreateView.as_view(),
-        name="api.bucketlistitem.create"),
-
-    url(r'^bucketlists/(?P<pk>[0-9]+)/items/(?P<pk_item>[0-9]+)$',
-        views.BucketlistItemDetailView.as_view(),
-        name="api.bucketlist.item")
-
+    url(r'^', include('restapi.urls'))
 ]
--- bucketlist/restapi/models.py
+++ bucketlist/restapi/models.py
@@ -5,31 +5,28 @@ from django.contrib.auth.models import User
 
 class BaseModel(models.Model):
     """Abstract model with bucketlist and items information"""
-    name = models.CharField(max_length=255)
+    name = models.CharField(blank=False, unique=True, max_length=255)
     date_created = models.DateTimeField(auto_now_add=True)
-    date_modified = models.DateTimeField(auto_now=True)
+    date_modified = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
 
 
-class UserProfile(BaseModel):
-    """A model representation of the user"""
-    bio = models.TextField(blank=True, null=True)
-    user = models.ForeignKey(User)
-
-
 class Bucketlist(BaseModel):
     """A model of the bucketlist table"""
-    user = models.ForeignKey(User)
+    created_by = models.ForeignKey(User)
 
-    def num_items_done(self):
-        """Returns number of bucketlist items completed"""
-        return self.bucketlist_set.filter(done=True).count()
+    def __str__(self):
+        return 'BucketList : {}'.format(self.name)
 
 
 class BucketlistItem(BaseModel):
     """A model of the Bucketlist item table"""
     done = models.BooleanField(default=False)
-    user = models.ForeignKey(User)
-    bucketlist = models.ForeignKey(Bucketlist)
+    created_by = models.ForeignKey(User)
+    bucketlist = models.ForeignKey(
+        Bucketlist, on_delete=models.CASCADE, related_name='items')
+
+    def __str__(self):
+        return 'Bucketlist Item : {}'.format(self.name)
