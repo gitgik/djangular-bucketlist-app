ededf6148d60d6fd4b2b49087f856fad01c76e9b
andela-ggikera <githinji.gikera@andela.com>
2016-01-29 12:25:12 +0300

    (test) Test deletion of bucketlist

--- bucketlist/restapi/tests.py
+++ bucketlist/restapi/tests.py
@@ -29,3 +29,59 @@ class UserAuthTestCase(SetUpMixin, APITestCase):
         """Test that the user can be authenticated to access service"""
         response = self.client.post('/auth/', data=self.user_data)
         self.assertContains(response, 'token', status_code=200)
+
+
+class BucketlistTestCase(SetUpMixin, APITestCase):
+    def test_user_can_create_bucketlist(self):
+        """Tests that the authenticated user can create a Bucketlist"""
+        rv = self.client.post('/auth/', data=self.user_data)
+        self.assertContains(rv, 'token', status_code=200)
+        jwt_token = json.loads(rv.content)
+        self.client.credentials(
+            HTTP_AUTHORIZATION='JWT {0}'.format(jwt_token.get('token')))
+        bucketlist_data = {'name': 'After I get to D1...'}
+        response = self.client.post(
+            reverse('api.bucketlists'), bucketlist_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_can_update_bucketlist(self):
+        pass
+
+    def test_user_can_delete_bucketlist(self):
+        """Ensures the use can delete an existing bucketlist"""
+        rv = self.client.post('/auth/', data=self.user_data)
+        self.assertContains(rv, 'token', status_code=200)
+        jwt_token = json.loads(rv.content)
+        self.client.credentials(
+            HTTP_AUTHORIZATION='JWT {0}'.format(jwt_token.get('token')))
+        bucketlist_data = {'name': 'Before I get married'}
+        response = self.client.post(
+            reverse('api.bucketlists'),
+            bucketlist_data,
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.delete(
+            reverse('api.bucketlist', kwargs={'pk': bucketlist.id}),
+            {},
+            format='json',
+            follow=True
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
