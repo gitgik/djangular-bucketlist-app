545d9471ba5baadf6cde85048ae89d7c9dfd0723
andela-ggikera <githinji.gikera@andela.com>
2016-01-29 10:18:51 +0300

    (test) Add user authentication test failing

--- bucketlist/restapi/tests.py
+++ bucketlist/restapi/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+from faker import Faker
+from .models import Bucketlist, BucketlistItem
+import json
 
-# Create your tests here.
+
+faker_instance = Faker()
+
+
+class SetupMixin(object):
+    """Create the setup for TestCases"""
+    @classmethod
+    def setUpClass(cls):
+        super(SetupMixin, cls).setUpClass()
+        cls.user_data = {
+            'username': 'jee',
+            'password': 'wordpass'
+        }
+        User.objects.create_user(**cls.user_data)
+        cls.client = APIClient()
+
+
+class UserAuthTestCase(SetupMixin, APITestCase):
+    def test_user_can_login(self):
+        """Test that the user can be authenticated to access service"""
+        response = self.client.post('/auth/register/', data=self.user_data)
+        self.assertContains(response, 'token', status_code=200)
