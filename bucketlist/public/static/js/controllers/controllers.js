14ef84f2f1f2d4c9e5f1c0989943f92c2218a310
andela-ggikera <githinji.gikera@andela.com>
2016-02-03 07:38:22 +0300

    (chore) Add menu service to delegate side navigation and toast functionality

--- bucketlist/public/static/js/controllers/controllers.js
+++ bucketlist/public/static/js/controllers/controllers.js
@@ -1,16 +1,8 @@
 'use strict';
 angular.module('bucketlist.controllers', ['ngMaterial'])
-.controller('AuthController', ['$rootScope', '$scope', '$state', '$localStorage', '$mdToast', 'BucketListService',
-    function AuthController($rootScope, $scope, $state, $localStorage, $mdToast, BucketListService) {
+.controller('AuthController', ['$rootScope', '$scope', '$state', '$localStorage', 'Toast', 'BucketListService',
+    function AuthController($rootScope, $scope, $state, $localStorage, Toast, BucketListService) {
 
-        var showToast = function (message) {
-            $mdToast.show(
-                $mdToast.simple()
-                    .textContent(message)
-                    .position($scope.getToastPosition())
-                    .hideDelay(2000)
-                );
-        }
         $scope.login = function () {
             var data = {username: $scope.user.username, password: $scope.user.password};
             BucketListService.auth.login(data).$promise.then(function(response){
@@ -20,10 +12,10 @@ angular.module('bucketlist.controllers', ['ngMaterial'])
                 $localStorage.currentUserid = response.id;
 
                 $state.go('dashboard');
-                showToast('Welcome ' + $scope.user.username);
+                Toast.show('Welcome ' + $scope.user.username);
             })
             .catch(function(responseError){
-                showToast('Incorrect credentials! Please try again.')
+                Toast.show('Incorrect credentials! Please try again.')
             });
 
         };
@@ -32,37 +24,13 @@ angular.module('bucketlist.controllers', ['ngMaterial'])
             var data = {username: $scope.user.username, password: $scope.user.password};
             BucketListService.users.create(data).$promise.then($scope.login)
             .catch(function(responseError){
-                $mdToast.show(
-                    $mdToast.simple()
-                        .textContent('Yikes! Could not register user.')
-                        .position($scope.getToastPosition())
-                        .hideDelay(2000)
-                );
+                Toast.show('Oops! User can\'t be registered')
             });
-        };
-        var last = {
-            bottom: false, top: true,
-            left: false, right: true
-        };
-        var sanitizePosition = function () {
-            var current = $scope.toastPosition;
-            if ( current.bottom && last.top ) current.top = false;
-            if ( current.top && last.bottom ) current.bottom = false;
-            if ( current.right && last.left ) current.left = false;
-            if ( current.left && last.right ) current.right = false;
-            last = angular.extend({},current);
-        }
-        $scope.toastPosition = angular.extend({},last);
-        $scope.getToastPosition = function() {
-            sanitizePosition();
-            return Object.keys($scope.toastPosition)
-              .filter(function(pos) { return $scope.toastPosition[pos]; })
-              .join(' ');
         };
     }])
 
-.controller('BucketListController', ['$rootScope', '$scope', '$state', '$localStorage', '$stateParams', '$mdToast', '$mdSidenav', '$timeout', 'BucketListService', '$mdDialog',
-    function BucketListController($rootScope, $scope, $state, $localStorage, $stateParams, $mdToast, $mdSidenav, $timeout, BucketListService, $mdDialog) {
+.controller('BucketListController', ['$rootScope', '$scope', '$state', '$localStorage', '$stateParams', 'Toast', '$mdSidenav', '$timeout', 'BucketListService', '$mdDialog', 'Menu',
+    function BucketListController($rootScope, $scope, $state, $localStorage, $stateParams, Toast, $mdSidenav, $timeout, BucketListService, $mdDialog, Menu) {
 
     $scope.selectedBucket = {};
     $scope.bucketlists = BucketListService.Bucketlists.getAllBuckets();
@@ -74,6 +42,11 @@ angular.module('bucketlist.controllers', ['ngMaterial'])
         $scope.selectedBucket = bucketlist
     };
 
+    $scope.toggleLeft = Menu.toggle('left');
+    $scope.close = function () {
+        $mdSidenav('left').close()
+            .then(function () {});
+    }
     // create a bucketlist using the provided name
     $scope.createBucketlist = function () {
         var data = { name: $scope.newbucket.name };
@@ -81,11 +54,11 @@ angular.module('bucketlist.controllers', ['ngMaterial'])
             function(response) {
                 // emit the trigger to a fresh UI update
                 $scope.$emit('updateBucketList');
-                showToast('Yeiy! Bucketlist created successfully!')
+                Toast.show('Yeiy! Bucketlist created successfully!')
                 // nullify the new bucketlist object
                 $scope.newbucket.name = null;
             }, function() {
-                showToast('Oops! There is a bucket with the same name.')
+                Toast.show('Oops! There is a bucket with the same name.')
             })
     }
 
@@ -95,49 +68,6 @@ angular.module('bucketlist.controllers', ['ngMaterial'])
         .then(function (response) {
 
         })
-    };
-
-    // helper functions for toast.
-    $scope.toggleLeft = buildDelayedToggler('left');
-    $scope.close = function () {
-      $mdSidenav('left').close()
-        .then(function () {});
-    };
-    /**
-     * Supplies a function that will continue to operate until the
-     * time is up.
-     */
-    function debounce(func, wait, context) {
-      var timer;
-      return function debounced() {
-        var context = $scope,
-            args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
-    }
-    /**
-     * Build handler to open/close a SideNav; when animation finishes
-     * report completion in console
-     */
-    function buildDelayedToggler(navID) {
-      return debounce(function() {
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {});
-      }, 200);
-    }
-
-    var showToast = function (message) {
-        $mdToast.show(
-            $mdToast.simple()
-                .textContent(message)
-                .position($scope.getToastPosition())
-                .hideDelay(2000)
-            );
     };
 
     $scope.showConfirm = function(ev, bucketlist) {
@@ -153,35 +83,16 @@ angular.module('bucketlist.controllers', ['ngMaterial'])
             bucketlist.$deleteBucket().then(function() {
                 delete $scope.selectedBucket;
                 $scope.$emit('updateBucketList');
-                showToast('Bucketlist deleted successfully')
+                Toast.show('Bucketlist deleted successfully')
             });
         }, function() {});
-    };
-
-    var last = {
-        bottom: false, top: true,
-        left: false, right: true
-    };
-    var sanitizePosition = function () {
-        var current = $scope.toastPosition;
-        if ( current.bottom && last.top ) current.top = false;
-        if ( current.top && last.bottom ) current.bottom = false;
-        if ( current.right && last.left ) current.left = false;
-        if ( current.left && last.right ) current.right = false;
-        last = angular.extend({},current);
-    }
-    $scope.toastPosition = angular.extend({},last);
-    $scope.getToastPosition = function () {
-        sanitizePosition();
-        return Object.keys($scope.toastPosition)
-          .filter(function(pos) { return $scope.toastPosition[pos]; })
-          .join(' ');
     };
 }])
 
 .controller('BucketListViewController', ['$rootScope', '$scope', '$state', '$localStorage', '$stateParams', '$mdToast', '$mdSidenav', '$timeout', 'BucketListService', '$mdDialog',
     function BucketListViewController ($rootScope, $scope, $state, $localStorage, $stateParams, $mdToast, $mdSidenav, $timeout, BucketListService, $mdDialog) {
 
+        $scope.bucketlists = BucketListService.Bucketlists.getAllBuckets();
         $scope.$on('updateBucketlist', function () {
             $scope.bucketlist= BucketListService.Bucketlists.getOneBucket({
                 id: $stateParams.id
@@ -193,6 +104,53 @@ angular.module('bucketlist.controllers', ['ngMaterial'])
             id: $stateParams.id
         });
 
+        $scope.createBucketItem = function (params) {
+            BucketListService.BucketlistItems.createBucketItem.$promise.then(
+                function(response) {
+                    $scope.newitem.name = null
+                    $scope.$emit('updateBucketlist');
+                    ('Item created successfully');
+                }, function() {
+                    //creating an item failed
+
+                })
+        };
+
+        /*
+        Helper functions for menu toggling.
+        */
+        $scope.toggleLeft = buildDelayedToggler('left');
+        $scope.close = function () {
+          $mdSidenav('left').close()
+            .then(function () {});
+        };
+        /**
+         * Supplies a function that will continue to operate until the
+         * time is up.
+         */
+        function debounce(func, wait, context) {
+          var timer;
+          return function debounced() {
+            var context = $scope,
+                args = Array.prototype.slice.call(arguments);
+            $timeout.cancel(timer);
+            timer = $timeout(function() {
+              timer = undefined;
+              func.apply(context, args);
+            }, wait || 10);
+          };
+        }
+        /**
+         * Build handler to open/close a SideNav; when animation finishes
+         * report completion in console
+         */
+        function buildDelayedToggler(navID) {
+          return debounce(function() {
+            $mdSidenav(navID)
+              .toggle()
+              .then(function () {});
+          }, 200);
+        }
     }])
 
 
