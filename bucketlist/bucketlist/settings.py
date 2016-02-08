3b2aa8354d3f9e203183784a69e6f201573b2bba
andela-ggikera <githinji.gikera@andela.com>
2016-02-08 11:20:31 +0300

    (chore) Change static root URL to staticfiles

--- bucketlist/bucketlist/settings.py
+++ bucketlist/bucketlist/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
 
